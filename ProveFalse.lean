5e1d2115615bdb4b82c6e4a5caa62ee35540d0c0
Martin Dvorak <martin.dvorak@matfyz.cz>
2026-07-29 14:42:46 +0200

    soundness bug in Lean 4.32.0 was found

--- ProveFalse.lean
+++ ProveFalse.lean
@@ -1,47 +1,65 @@
--- Mario Carneiro found this bug.
--- https://leanprover.zulipchat.com/#narrow/channel/270676-lean4/topic/Soundness.20bug.3A.20hasLooseBVars.20is.20not.20conservative/near/521286338
+-- Ramana Kumar discovered this bug.
+-- https://github.com/leanprover/lean4/issues/14576
 import Lean
-open Lean
+open Lean Elab Command
 
-def isProp.{u} : Prop :=
-  ∀ s : Sort u, ∀ x y : s, x = y
+inductive P : Prop where | mk (b : Bool)
+structure C where b : Bool
+inductive W : Type where | mk (p : P)
+inductive L (α : Type) (b : Bool) : Type where | mk
+inductive T : Bool → Prop where | mk : T true
 
-theorem isProp_prop : isProp.{0} :=
-  fun _ _ _ => rfl
+def pad (e : Expr) (n : Nat) : Expr :=
+  mkApp (mkLambda `x .default (mkConst ``Nat) e) (.lit (.natVal n))
 
-theorem not_isProp_type : ¬isProp.{1} :=
-  (nomatch · _ 0 1)
+meta def build : CommandElabM Unit := do
+  let f := pad (mkConst ``Bool.false) 78670
+  let t := pad (mkConst ``Bool.true) 24083
+  unless f.hash == t.hash && f.approxDepth == t.approxDepth do
+    throwError "hash collision failed"
+  let fw := mkApp (mkConst ``W.mk) (mkApp (mkConst ``P.mk) f)
+  let tw := mkApp (mkConst ``W.mk) (mkApp (mkConst ``P.mk) t)
+  let w := mkBVar 0
+  let Ew := mkApp (mkConst `E) w
+  let b := mkProj ``C 0 (mkProj ``C 0 w)
+  let l := mkApp2 (mkConst ``L) Ew b
+  let Et := mkForall `w .default (mkConst ``W) (mkSort 1)
+  let ct := mkForall `w .default (mkConst ``W) <|
+    mkForall `l .default l (mkApp (mkConst `E) (mkBVar 1))
+  liftCoreM <| addDecl <| .inductDecl [] 1 [{
+    name := `E, type := Et, ctors := [{ name := `E.mk, type := ct }] }] false
+  let Et := mkApp (mkConst `E) tw
+  let l := mkApp2 (mkConst ``L.mk) Et (mkConst ``Bool.true)
+  liftCoreM <| addDecl <| .defnDecl {
+    name := `e, levelParams := [], type := Et,
+    value := mkApp2 (mkConst `E.mk) tw l,
+    hints := .abbrev, safety := .safe }
+  liftCoreM <| addDecl <| .defnDecl {
+    name := `good', levelParams := [],
+    type := mkApp (mkConst ``T) t, value := mkConst ``T.mk,
+    hints := .abbrev, safety := .safe }
+  let Ef := mkApp (mkConst `E) fw
+  let Et := mkApp (mkConst `E) tw
+  let a := mkApp2 (mkConst `E.mk) fw (mkProj `E 0 (mkConst `e))
+  let tl := mkApp2 (mkConst ``L.mk) Et (mkConst ``Bool.true)
+  let b := mkApp2 (mkConst `E.mk) tw tl
+  let cT := mkApp2 (mkConst ``L) Et t
+  let c := mkApp (mkLambda `l .default cT (mkConst ``Unit.unit)) tl
+  let fl := mkApp2 (mkConst ``L.mk) Ef f
+  let d := mkApp2 (mkConst `E.mk) fw fl
+  let v := Expr.letE `a Ef a
+    (.letE `b Et b
+      (.letE `c (mkConst ``Unit) c
+        (.letE `d Ef d (mkConst `good') true) true) true) true
+  liftCoreM <| addDecl <| .thmDecl {
+    name := `bad, levelParams := [],
+    type := mkApp (mkConst ``T) f, value := v }
 
-theorem isProp_not_invariant : isProp.{0} ≠ isProp.{1} :=
-  mt (cast · isProp_prop) not_isProp_type
-
-def mkLevel : Nat → Level → Level
-  | 0  , e => e
-  | n+1, e => mkLevel n (.max 0 e)
-
-#guard_msgs(drop all) in
-run_elab
-  Lean.addDecl <| .defnDecl {
-    name := `magic
-    levelParams := []
-    type := .sort .zero
-    value := .const `isProp [mkLevel (2^24) (.param `u)]
-    hints := .opaque
-    safety := .safe
-  }
-
-run_elab
-  Lean.addDecl <| .defnDecl {
-    name := `magic_eq
-    levelParams := [`u]
-    type := mkPropEq (.const `magic []) (.const `isProp [.param `u])
-    value := mkApp2 (mkConst ``Eq.refl [levelOne]) (.sort .zero) (.const `magic [])
-    hints := .opaque
-    safety := .safe
-  }
+elab "mkbug" : command => build
+mkbug
 
 theorem contradictio : False :=
-  isProp_not_invariant (magic_eq.{0}.symm.trans magic_eq.{1})
+  nomatch (bad : T false)
 
 #print axioms contradictio
 
